1350c2405a498f6c60f6de8f6fcd63522d3d113b
Ognjen Lazic <laogdo@gmail.com>
2016-08-12 16:23:44 +0200

    Code cleanup

--- scan_network.py
+++ scan_network.py
@@ -1,12 +1,10 @@
 import subprocess
-#using subprocess to spawn new proces that is sending
-# arp-scan -l command on terminal and gets output of that
-#process.
+
+
 
 def get_everyone():
     batcmd = "sudo arp-scan -l"
     return subprocess.check_output(batcmd, shell=True)
-
 
 def find_ps3(ps3_mac_address):
     if ps3_mac_address in get_everyone(): return True
